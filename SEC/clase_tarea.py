3c91b02748ef57f0e70a56434ed70ce1bce4ba55
snopv <snop.vlc@gmail.com>
2021-05-02 20:56:33 +0200

    ELIMINACION: SEC

--- SEC/clase_tarea.py
+++
@@ -1,9 +0,0 @@
-class PerTask:
-    def __init__(self, id, prio, per, D):
-        self.id = id
-        self.periodo = per
-        self.prio = prio
-        self.deadline = D
-
-    def print_param(self):
-        print("Id: " + str(self.id) + " Per: " + str(self.periodo)+ " Prio: " +str(self.prio)+ " Deadline: " + str(self.deadline))
