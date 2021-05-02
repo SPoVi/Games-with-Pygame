95511ee9f129a0788ae30d5db7ee43bb5b25eb83
snopv <snop.vlc@gmail.com>
2021-05-02 15:46:21 +0200

    MODIFICACION - introducir_tarea_periodica.py Pide cantidad de tareas. Pide sus periodos y tiempos de computo.

--- SEC/introducir_tarea_periodica.py
+++ SEC/introducir_tarea_periodica.py
@@ -1,7 +1,14 @@
 '''
 Partimos de tarea_periodica.py
 
+PASO 1:
 A diferencia del otro codigo aqui los datos de la tarea se introducen por el terminal
+
+PASO 2:
+Introducir otra tarea
+
+PASO 3:
+Crear una clase "Tarea"
 '''
 import pygame, sys
 import pygame.locals as GAME_GLOBALS
@@ -18,38 +25,57 @@ WIN_HEIGHT = 500
 surface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT)) # ventana
 pygame.display.set_caption('Codigo 2: Tarea Periodica.')
 
+# TERMINAL
+print("Introduce la cantidad de tareas: ")
+numTareas = int(input())
+i = 0
+lista_periodo = list()
+lista_computo = list()
+
+for i in range(0,numTareas):
+    # PEDIR DATOS
+    print(f"Periodo de la tarea {i+1}: ")
+    periodo = int(input())
+    print(f"Tiempo de computo de la tarea (tiempo max de ejecución) {i}: ")
+    computo = int(input())
+
+    lista_periodo.append(periodo)
+    lista_computo.append(computo)
+
 # Variables cosntantes de la tareas
 y1 = 10
 x1 = 10
-sep = 10
+sep = 10 # separacion entre dibujos cada segundo
+
+print("Entrando en posicionamiento inicial")
 # Donde empieza el cuadrado
-tareaX = 0
-tareaY = WIN_HEIGHT / 2
+lista_TareaX = list()
+lista_TareaY = list()
+lista_cont = list()
+for i in range(0,numTareas):
+    tareaX = 0
+    tareaY = WIN_HEIGHT / 2 + 20*(i-1)
+
+    lista_TareaX.append(tareaX)
+    lista_TareaY.append(tareaY)
+    lista_cont.append(0)
 
 
 surface.fill((0,0,0)) # Fondo negro
 
-# PEDIR DATOS
-print("Periodo de la tarea: ")
-periodo = int(input())
-print("Tiempo de computo de la tarea (tiempo max de ejecución): ")
-computo = int(input())
-
-
-cont = 0
-
 # loop infinito
 while True:
+    # Param del rect: (ventana, color, x0, y0, x1, y1)
+    for i in range(0,numTareas):
 
-    #Param del rect: (ventana, color, x0, y0, x1, y1)
-    if cont < computo:
-        pygame.draw.rect(surface, (255, 255, 255), (tareaX, tareaY, x1, y1))  # Duracion (blanco)
-        tareaX+= 10
-        cont+= 1
-    elif cont == computo:
-        pygame.draw.rect(surface, (0, 0, 0), (tareaX, tareaY, x1, y1))  # Espacio (negro)
-        tareaX += 10
-        cont = 0
+        if lista_cont[i] < lista_computo[i]: # Duracion (blanco)
+            pygame.draw.rect(surface, (255, 255, 255), (int(lista_TareaX[i]), int(lista_TareaY[i]), x1, y1))
+            lista_TareaX[i] += sep
+            lista_cont[i] += 1
+        elif lista_cont[i] == lista_computo[i]:
+            pygame.draw.rect(surface, (0, 0, 0), (int(lista_TareaX[i]), int(lista_TareaY[i]), x1, y1))
+            lista_TareaX[i] += sep
+            lista_cont[i] = 0
 
     for event in GAME_EVENTS.get():
         if event.type == GAME_GLOBALS.QUIT:
